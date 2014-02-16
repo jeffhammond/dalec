b0f00642505cb660bb277ddf5f3c146c19f35c7d
Jeff Hammond <jeff.science@gmail.com>
2014-02-15 19:40:19 -0600

    rest of ARMCI to DALEC conversion

--- tests/Makefile.mk
+++ tests/Makefile.mk
@@ -2,14 +2,15 @@
 # Copyright (C) 2014. See COPYRIGHT in top-level directory.
 #
 
-check_PROGRAMS += tests/test_rmw_fadd         \
+check_PROGRAMS += tests/test_hello            \
+		  tests/test_assert           \
                   # end
 
-TESTS          += tests/test_rmw_fadd         \
+TESTS          += tests/test_hello            \
                   # end
 
 XFAIL_TESTS    += tests/test_assert           \
                   # end
 
-tests_test_assert_LDADD = libarmci.la
-tests_test_rmw_fadd_LDADD = libarmci.la
+tests_test_assert_LDADD = libdalec.la
+tests_test_hello_LDADD = libdalec.la
