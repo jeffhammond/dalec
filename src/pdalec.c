a3d5d42cdbe0c0b024f8bda573c1ee34f812f516
Jeff Hammond <jeff.science@gmail.com>
2014-02-15 19:45:27 -0600

    weak symbols not working right now on Mac

---
+++ src/pdalec.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2014. See COPYRIGHT in top-level directory.
+ */
+
+/* If no weak symbols support */
+#if !defined(HAVE_PRAGMA_WEAK) && !defined(HAVE_PRAGMA_HP_SEC_DEF) && !defined(HAVE_PRAGMA_CRI_DUP)
+
+#include "armci.h"
+
+#pragma weak DALEC_Initialize
+int DALEC_Initialize(void) {
+  return PDALEC_Initialize();
+}
+
+#pragma weak DALEC_Finalize
+int DALEC_Finalize(void) {
+  return PDALEC_Finalize();
+}
+
+#endif
