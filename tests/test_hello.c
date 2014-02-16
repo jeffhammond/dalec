cd74ac17f40869b568385aaa04e64438c44afd3b
Jeff Hammond <jeff.science@gmail.com>
2014-02-15 19:38:50 -0600

    rename and convert to simple program in DALEC

---
+++ tests/test_hello.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2014. See COPYRIGHT in top-level directory.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include <mpi.h>
+#include <dalec.h>
+
+int main(int argc, char ** argv) {
+
+    int rank, nproc;
+
+    MPI_Init(&argc, &argv);
+    DALEC_Initialize();
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+
+    if (rank == 0) printf("Starting DALEC Init/Final test with %d processes\n", nproc);
+
+    DALEC_Finalize();
+    MPI_Finalize();
+
+    return 0;
+}
