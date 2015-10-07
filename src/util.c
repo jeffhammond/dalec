d6b171d9031044e05bdcd20f0aa1b62bd59f68f9
Jeff Hammond <jeff.science@gmail.com>
2015-10-07 07:01:14 -0700

    reformatting and global state changes

--- src/util.c
+++ src/util.c
@@ -14,46 +14,50 @@
 /** Fatal error, print the message and abort the program with the provided
   * error code.
   */
-void DALEC_Error(char *msg, int code) {
+void DALEC_Error(char *msg, int code)
+{
     int rank;
-    MPI_Comm_rank(DALEC_COMM_WORLD, &rank);
+    MPI_Comm_rank(DALECI_GLOBAL_STATE.DALEC_COMM_WORLD, &rank);
 
     fprintf(stderr, "[%d] DALEC Error: %s\n", rank, msg);
     fflush(NULL);
 
-    MPI_Abort(DALEC_COMM_WORLD, code);
+    MPI_Abort(DALECI_GLOBAL_STATE.DALEC_COMM_WORLD, code);
 
     return;
 }
 
 /** Retrieve the value of a boolean environment variable.  */
-int DALECI_Getenv_bool(char *varname, int default_value) {
+int DALECI_Getenv_bool(char *varname, int default_value)
+{
   char *var = getenv(varname);
 
-  if (var == NULL)
+  if (var == NULL) {
     return default_value;
-
-  if (var[0] == 'T' || var[0] == 't' || var[0] == '1' || var[0] == 'y' || var[0] == 'Y')
+  } else if (var[0] == 'T' || var[0] == 't' || var[0] == '1' || var[0] == 'y' || var[0] == 'Y') {
     return 1;
-
-  else
+  } else {
     return 0;
+  }
 }
 
 
 /** Retrieve the value of a environment variable.
   */
-char *DALECI_Getenv(char *varname) {
+char *DALECI_Getenv(char *varname)
+{
   return getenv(varname);
 }
 
 
 /** Retrieve the value of an integer environment variable.
   */
-int DALECI_Getenv_int(char *varname, int default_value) {
+int DALECI_Getenv_int(char *varname, int default_value)
+{
   char *var = getenv(varname);
-  if (var)
+  if (var) {
     return atoi(var);
-  else
+  } else {
     return default_value;
+  }
 }
